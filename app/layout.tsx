f12023442e742af3dbf0175ba75198d60529b8a4
rof1yev <rofiyevdilshod@gmail.com>
2024-08-10 14:23:28 +0500

    Google search meta data added

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,16 @@ export const metadata: Metadata = {
     { name: "Dilshod Rofiyev", url: "https://www.instagram.com/rof1yev/" },
   ],
   applicationName: "Movie Application",
-  keywords: "movie, tv, serial, kino",
+  keywords:
+    "Netflix, Online streaming, Movies and TV shows, Original series, Watch online, HD streaming, Binge-watch, On-demand content, Subscription service, Entertainment, Documentaries, Netflix originals, Family movies, Drama series, Action movies, Comedy shows, Streaming platform, TV series, Popular shows, Netflix subscription, New releases, Watch anywhere, Ad-free streaming, Movie recommendations, Exclusive content",
+  verification: {
+    google: "google",
+    yandex: "yandex",
+    yahoo: "yahoo",
+    other: {
+      me: ["rofiyevdilshod@gmail.com", "https://www.instagram.com/rof1yev/"],
+    },
+  },
   icons: [
     {
       url: "/netflix-icon.svg",
@@ -33,6 +42,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta
+          name="google-site-verification"
+          content="TWhx17k6Q6zi64nwcE1mT0QBcnOPl-ARF1TRx_lWQVA"
+        />
+      </head>
       <body className={ubuntu.className}>
         <Provider attribute="class" defaultTheme="dark">
           <GlobalContext>{children}</GlobalContext>
