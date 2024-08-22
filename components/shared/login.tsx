0f4585eda9502916c5f22539ae66ded63848d12d
rof1yev <rofiyevdilshod@gmail.com>
2024-08-22 12:09:29 +0500

    login page change design

--- components/shared/login.tsx
+++ components/shared/login.tsx
@@ -5,12 +5,18 @@ import { Button } from "../ui/button";
 import { AiFillGithub } from "react-icons/ai";
 import { FaGoogle } from "react-icons/fa6";
 import { signIn } from "next-auth/react";
+import { FcGoogle } from "react-icons/fc";
 
 const Login = () => {
   return (
     <div className="w-full h-screen">
       <div className="absolute inset-0 bg-slate-950">
-        <Image src="/background.jpg" alt="image" fill className="opacity-50 object-cover" />
+        <Image
+          src="/background.jpg"
+          alt="image"
+          fill
+          className="opacity-50 object-cover"
+        />
       </div>
       <div className="relative z-10 w-11/12 md:w-[600px] bg-black/60 rounded-md h-[50vh] left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 px-8 py-4">
         <div className="flex items-center flex-col justify-center h-full">
@@ -22,14 +28,14 @@ const Login = () => {
           </p>
 
           <Button
-            className="mt-4 flex items-center w-full h-10 sm:h-[56px] rounded !bg-red-600 !text-white hover:opacity-90 transition"
+            className="mt-4 flex items-center w-full h-10 sm:h-[56px] rounded bg-white text-neutral-800 hover:bg-gray-100 transition"
             onClick={() => signIn("google")}
           >
-            <FaGoogle className="w-6 sm:w-7 h-6 sm:h-7 mr-2" />
+            <FcGoogle className="w-6 sm:w-7 h-6 sm:h-7 mr-2" />
             Sign in with Google
           </Button>
           <Button
-            className="mt-4 flex items-center w-full h-10 sm:h-[56px] rounded !bg-gray-400 !text-white hover:opacity-90 transition"
+            className="mt-4 flex items-center w-full h-10 sm:h-[56px] rounded !bg-gray-400 !text-white hover:!bg-gray-500 transition"
             onClick={() => signIn("github")}
           >
             <AiFillGithub className="w-6 sm:w-7 h-6 sm:h-7 mr-2" />
